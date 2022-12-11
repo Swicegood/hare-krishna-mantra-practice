a355a98aabc8aa17bcc8009b96fc513dd10ad9b0
Swicegood <jguru108@gmail.com>
2022-12-10 21:22:29 -0500

    loop though textviews

--- app/src/main/java/com/example/harekrishnamantrapractice/FirstFragment.kt
+++ app/src/main/java/com/example/harekrishnamantrapractice/FirstFragment.kt
@@ -1,10 +1,12 @@
 package com.example.harekrishnamantrapractice
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 import com.example.harekrishnamantrapractice.databinding.FragmentFirstBinding
 
@@ -32,7 +34,15 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val packageName = context!!.packageName
+        for (i in 0..0) {
+            val tview = getView()?.findViewById<TextView>(R.id.textview0+i)
+            tview?.textSize = 36f
+            Log.d("Heeeeeeeeeeeeeeeeeeeeeeeey here is the tvew", tview.toString())
+        }
+
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
